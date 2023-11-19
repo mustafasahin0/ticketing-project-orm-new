3d922c66dae7f1cc2e7a9015cac2071d5777fdd0
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-11-18 23:02:53 -0700

    Part-1

--- src/main/java/com/example/entity/Role.java
+++ src/main/java/com/example/entity/Role.java
@@ -1,14 +1,21 @@
 package com.example.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-public class Role {
-    private Long id;
+@Entity
+@Table(name = "roles")
+public class Role extends BaseEntity{
+
     private String description;
 
 }
