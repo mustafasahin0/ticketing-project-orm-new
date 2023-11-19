b1b3c1d80c46a3b91a639d87e6e6bbed8472c258
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-11-19 15:05:51 -0700

    Part-2

---
+++ src/main/java/com/example/mapper/UserMapper.java
@@ -0,0 +1,23 @@
+package com.example.mapper;
+
+import com.example.dto.UserDTO;
+import com.example.entity.User;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+    private final ModelMapper modelMapper;
+
+    public UserMapper(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    public UserDTO convertToDTO(User user) {
+        return modelMapper.map(user, UserDTO.class);
+    }
+
+    public User convertToEntity(UserDTO userDTO) {
+        return modelMapper.map(userDTO, User.class);
+    }
+}
--- src/main/java/com/example/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/service/impl/UserServiceImpl.java
@@ -1,16 +1,29 @@
 package com.example.service.impl;
 
 import com.example.dto.UserDTO;
+import com.example.entity.User;
+import com.example.mapper.UserMapper;
+import com.example.repository.UserRepository;
 import com.example.service.UserService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper) {
+        this.userRepository = userRepository;
+        this.userMapper = userMapper;
+    }
+
     @Override
     public List<UserDTO> listAllUsers() {
-        return null;
+        return userRepository.findAll().stream().map(userMapper::convertToDTO).collect(Collectors.toList());
     }
 
     @Override
