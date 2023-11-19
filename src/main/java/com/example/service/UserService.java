df01fba1d38fd587acce4e6c65a28031d6bf52f6
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-11-18 21:36:22 -0700

    Initial commit

---
+++ src/main/java/com/example/service/UserService.java
@@ -0,0 +1,13 @@
+package com.example.service;
+
+import com.example.dto.UserDTO;
+
+import java.util.List;
+
+public interface UserService extends CrudService<UserDTO, String> {
+
+    List<UserDTO> findManagers();
+
+    List<UserDTO> findEmployees();
+
+}
