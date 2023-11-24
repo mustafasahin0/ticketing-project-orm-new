548ec97a2d25d129ebd77414c28f8527cb2e000d
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-11-23 20:33:57 -0700

    Part-2

--- src/main/java/com/example/repository/UserRepository.java
+++ src/main/java/com/example/repository/UserRepository.java
@@ -3,8 +3,12 @@ package com.example.repository;
 import com.example.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByUserName(String username);
+
+    @Transactional
+    void deleteByUserName(String username);
 }
--- src/main/java/com/example/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/service/impl/UserServiceImpl.java
@@ -53,6 +53,6 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void deleteByUserName(String userName) {
-
+        userRepository.deleteByUserName(userName);
     }
 }
